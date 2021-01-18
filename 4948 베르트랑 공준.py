a9d71a9ba3d76d3b7f72e0b30819a8aa7b9bec9d
OseungKwon <54261116+OseungKwon@users.noreply.github.com>
2021-01-18 15:02:23 +0900

    Update 4948 베르트랑 공준.py

--- 4948 베르트랑 공준.py
+++ 4948 베르트랑 공준.py
@@ -1,19 +1,25 @@
-while True:
-    x=int(input())
-    if(x==0):
-        break
+def calc(x):
     count = 0
+    m=int(x**0.5)
     arr = [True]*x
 
-    for i in range(2, )
-    for i in range(len(arr)):
-        if (arr[i] != 0):
-            for j in range(i + 1, len(arr)):
-                if (arr[j] % arr[i] == 0):
-                    arr[j] = 0
-    for i in range(x+1,len(arr)):
-        if(arr[i]!=0):
+    for i in range(2, m+1):
+        if(arr[i]==True):
+            for j in range(i+i,x,i): #i*2부터는 False로
+                arr[j]=False
+    for i in range(2,x):
+        if(arr[i]==True):
             count+=1
+    return(count)
 
-    print(count)
+while True:
+    x=int(input())
+    if x==0:
+        break
+    if x==1:
+        print(1)
+    else:
+        print(calc(x*2+1)-calc(x+1))
+
+
 
