331b1f994cba217d37ef66c39d8c01658fba23f2
OseungKwon <54261116+OseungKwon@users.noreply.github.com>
2021-02-26 14:05:38 +0900

    add

--- 9663 N-Queen.py
+++ 9663 N-Queen.py
@@ -1,1 +1,25 @@
 n=int(input())
+arr=[]
+def available(x,col):
+
+    row=len(x)
+    for queen_row in range(row):
+        if x[queen_row]==col or abs(x[queen_row]-col)==row-queen_row:
+            return False
+    print(x,col,row)
+    return True
+
+
+
+def DFS(n,row,x,arr):
+    if row==n:
+        arr.append(x[:])
+        #print(arr)
+        return
+    for col in range(n):
+        if available(x,col):
+            x.append(col)
+            DFS(n,row+1,x,arr)
+            x.pop()
+DFS(n,0,[],arr)
+print(len(arr))
--- sample.py
+++ sample.py
@@ -1,9 +1,5 @@
-n=int(input())
-arr=[]
-for _ in range(n):
-    age,name=input().split()
-
-    arr.append((age,name))
-arr.sort(key=lambda x:int(x[0]))
-for i,j in arr:
-    print(i,j)
+def DFS(x):
+    global result
+    if x==n:
+        result+=1
+    else
