ae7566b2b72bca62d33e7fe94f293dc61f812e37
OseungKwon <54261116+OseungKwon@users.noreply.github.com>
2021-03-05 17:53:49 +0900

    Update 14889 스타트와 링크.py

--- 14889 스타트와 링크.py
+++ 14889 스타트와 링크.py
@@ -1,5 +1,5 @@
 n=int(input())
-arr=[list(map(int,input().split())) for _ in range(4)]
+arr=[list(map(int,input().split())) for _ in range(n)]
 print(arr)
 team_set={i for i in range(n)}
 s=[]
@@ -13,8 +13,22 @@ def solve(depth,count):
       continue
     s.append(i)
     solve(depth+1,count)
-    #count += 1
+    count += 1
     s.pop()
 solve(0,0)
 team=[empty[i:i + 2] for i in range(0, len(empty), 2)]
 print(team)
+
+def fin(team,em):
+  for i,j in team:
+    print(i,j)
+    k=arr[i][j]+arr[j][i]
+    print(em)
+    em.append(k)
+em=[]
+fin(team,em)
+min=abs(em[0]-em[1])
+for i in range(0,len(em)-1):
+  if min>abs(em[i]-em[i+1]):
+    min=abs(em[i]-em[i+1])
+print(min)
